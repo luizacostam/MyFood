6ea6166f9e0f37c426ae53713787a752180f1823
Luiza Costa <113187633+luizacostam@users.noreply.github.com>
2024-09-02 00:11:53 -0300

    Update UtilsFileReader.java

--- src/br/ufal/ic/p2/myfood/utils/UtilsFileReader.java
+++ src/br/ufal/ic/p2/myfood/utils/UtilsFileReader.java
@@ -4,8 +4,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 
 import br.ufal.ic.p2.myfood.Sistema;
+import br.ufal.ic.p2.myfood.Exception.CategoriaInvalidaException;
 import br.ufal.ic.p2.myfood.Exception.ContaComEsseEmailJaExisteException;
 import br.ufal.ic.p2.myfood.Exception.CpfInvalidoException;
 import br.ufal.ic.p2.myfood.Exception.CpfNaoPodeSerNuloException;
@@ -14,19 +16,22 @@ import br.ufal.ic.p2.myfood.Exception.EmpresaComEsseNomeELocalJaExisteException;
 import br.ufal.ic.p2.myfood.Exception.EmpresaComEsseNomeJaExisteException;
 import br.ufal.ic.p2.myfood.Exception.EnderecoNaoPodeSerNuloException;
 import br.ufal.ic.p2.myfood.Exception.FormatoDeEmailInvalidoException;
+import br.ufal.ic.p2.myfood.Exception.NomeInvalidoException;
 import br.ufal.ic.p2.myfood.Exception.NomeNaoPodeSerNuloException;
+import br.ufal.ic.p2.myfood.Exception.ProdutoComEsseNomeJaExisteException;
 import br.ufal.ic.p2.myfood.Exception.SenhaNaoPodeSerNulaException;
 import br.ufal.ic.p2.myfood.Exception.UsuarioNaoPodeCriarUmaEmpresaException;
+import br.ufal.ic.p2.myfood.Exception.ValorInvalidoException;
 import br.ufal.ic.p2.myfood.models.Dono;
 import br.ufal.ic.p2.myfood.models.Usuario;
 
-
 public class UtilsFileReader {
-	public static void lerArquivos(Sistema sistema) throws EnderecoNaoPodeSerNuloException, NomeNaoPodeSerNuloException, CpfNaoPodeSerNuloException, ContaComEsseEmailJaExisteException, EmailInvalidoException, FormatoDeEmailInvalidoException, SenhaNaoPodeSerNulaException, CpfInvalidoException, UsuarioNaoPodeCriarUmaEmpresaException, EmpresaComEsseNomeELocalJaExisteException, EmpresaComEsseNomeJaExisteException {
+	public static void lerArquivos(Sistema sistema) throws EnderecoNaoPodeSerNuloException, NomeNaoPodeSerNuloException, CpfNaoPodeSerNuloException, ContaComEsseEmailJaExisteException, EmailInvalidoException, FormatoDeEmailInvalidoException, SenhaNaoPodeSerNulaException, CpfInvalidoException, UsuarioNaoPodeCriarUmaEmpresaException, EmpresaComEsseNomeELocalJaExisteException, EmpresaComEsseNomeJaExisteException, NomeInvalidoException, ValorInvalidoException, CategoriaInvalidaException, ProdutoComEsseNomeJaExisteException {
         lerArquivo("usuarios", sistema);
         lerArquivo("empresas", sistema);
+        lerArquivo("produtos", sistema);
 	}
-	public static void lerArquivo(String arquivo, Sistema sistema) throws EnderecoNaoPodeSerNuloException, NomeNaoPodeSerNuloException, CpfNaoPodeSerNuloException, ContaComEsseEmailJaExisteException, EmailInvalidoException, FormatoDeEmailInvalidoException, SenhaNaoPodeSerNulaException, CpfInvalidoException, UsuarioNaoPodeCriarUmaEmpresaException, EmpresaComEsseNomeELocalJaExisteException, EmpresaComEsseNomeJaExisteException {
+	public static void lerArquivo(String arquivo, Sistema sistema) throws EnderecoNaoPodeSerNuloException, NomeNaoPodeSerNuloException, CpfNaoPodeSerNuloException, ContaComEsseEmailJaExisteException, EmailInvalidoException, FormatoDeEmailInvalidoException, SenhaNaoPodeSerNulaException, CpfInvalidoException, UsuarioNaoPodeCriarUmaEmpresaException, EmpresaComEsseNomeELocalJaExisteException, EmpresaComEsseNomeJaExisteException, NomeInvalidoException, ValorInvalidoException, CategoriaInvalidaException, ProdutoComEsseNomeJaExisteException {
         File file = new File("./database/" + arquivo + ".txt");
 
         if (!file.exists()) return;
@@ -40,6 +45,7 @@ public class UtilsFileReader {
 
                 if(arquivo.equals("usuarios")) lerUsuarios(sistema, dados);
                 if(arquivo.equals("empresas")) lerEmpresas(sistema, dados);
+                if(arquivo.equals("produtos")) lerProdutos(sistema, dados);
             }
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo " + arquivo);
@@ -85,14 +91,37 @@ public class UtilsFileReader {
 			String tipoEmpresa = dados_atuais[4];
 			
 			int dono = Integer.valueOf(donoId);
-	
+//			System.out.println(dono);
+//			if (dono >= 3) {
+//				return;
+//			}
 			Usuario usuario = sistema.getUsuarioById(dono);
 			
-			if(usuario.getClass().equals(Dono.class)) {				
+			if(usuario instanceof Dono) {				
 				sistema.criarEmpresa(tipoEmpresa, dono, nome, endereco, tipoEmpresa);
 			} else {
 				throw new UsuarioNaoPodeCriarUmaEmpresaException();
 			}
+			
+		}
+	}
+	
+	private static void lerProdutos(Sistema sistema, String[] dados) throws NomeInvalidoException, ValorInvalidoException, CategoriaInvalidaException, ProdutoComEsseNomeJaExisteException {
+		String[] dados_atuais = null;
+		String[] var3 = dados;
+		int var4 = dados.length;
+		
+		for (int var5 = 0; var5 < var4; ++var5) {
+			String dado = var3[var5];
+			dados_atuais = dado.split(":");
+			String id = dados_atuais[0];
+			String nome = dados_atuais[1];
+			String valor = dados_atuais[2];
+			String categoria = dados_atuais[3];
+			
+			int empresaId = Integer.parseInt(id);
+			float preco = Float.parseFloat(valor);
+			sistema.criarProduto(empresaId, nome, preco, categoria);
 			
 		}
 	}
